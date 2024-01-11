d02811e54646dee7a7ad38130e90d666ab4ab582
kushal <kushal.p@medkart.in>
2024-01-11 19:05:27 +0530

    test commit

---
+++ edit-delete.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+<head>
+	<title>Edit Delete Task</title>
+<?php
+
+$servername="localhost";
+$username="user";
+$password="password";
+$dbname="todo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	echo var_dump($_REQUEST);
+}
+?>
+
+<style>
+table tr td{
+border: 1px solid black;
+}
+
+table { 
+    border-spacing: 0px;
+    border-collapse: separate;
+}
+</style>
+
+</head>
+<body>
+
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+<?php
+
+$sql=sprintf("select * from tasks");
+echo "<table>";
+$result = $conn->query($sql);
+
+$value=0;
+while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+echo sprintf ("<tr><td>%s</td><td>%s</td><td><button type=\"submit\" name=\"edit\" value=\"%s\"></button></td><td><input type=\"submit\" name=\"delete\" value=\"DELETE\"></td>", $row["title"], $row["description"]);
+$value+=1;
+}
+
+echo "</table>";
+?>
+</form>
+
+
+<?PHP $conn->close(); ?>
+</body>
+</html>
