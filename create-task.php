b69b86f2676b006d4034117ca10aaf2d95c41069
kushal <kushal.p@medkart.in>
2024-01-11 16:31:26 +0530

    lets first try to complete task as soon as possi.

---
+++ create-task.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Title of the document</title>
+    <style>
+      input,
+      textarea {
+        padding: 5px;
+      }
+      span {
+        display: block;
+        padding: 15px 0 3px;
+      }
+      input:focus,
+      textarea:focus,
+      select:focus {
+        outline: none;
+      }
+    </style>
+  </head>
+  <body>
+    <h3>NEW TASK CREATION</h3>
+    <form>
+      <span>An input field:</span>
+      <input type="text">
+      <span>A textarea field:</span>
+      <textarea></textarea>
+	<br>
+	<br>
+	<input type=submit value=submit>
+    </form>
+  </body>
+</html>
---
+++ taskcreate.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="taskcreate.css">
+</head>
+<body>
+<div class="banner">
+	<div class="flexcontainer">
+	<img alt="todo">
+	<div class="todonet">Todo.net</div>
+	</div>
+</div>
+
+      <!--<input type="text" id="myOutline" placeholder="Enter Something" /> -->
+
+</body>
+</html>
