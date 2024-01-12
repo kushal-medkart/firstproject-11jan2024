092fff6b42aadd635208834803e6788a47d5af82
kushal <kushal.p@medkart.in>
2024-01-12 15:20:58 +0530

    now good front end is ready

--- create-task.php
+++ create-task.php
@@ -16,6 +16,44 @@
       select:focus {
         outline: none;
       }
+.createtaskbox {
+	background-color: black; 
+	border: 2px solid #9c7248;
+}
+
+
+ .createtaskbox input[type=text]{
+	background-color: black;
+	border-top: 0;
+	border-right: 0;
+	border-left: 0;
+	border-bottom: 2px white blue;
+	color: white;
+	margin: 8px;
+}
+ .createtaskbox textarea{
+	background-color: black;
+	border-top: 0;
+	border-right: 0;
+	border-left: 0;
+	color: white;
+	border-bottom: 2px white blue;
+}
+
+.createtaskbox {
+	padding: 40px;
+}
+.createtaskbox span{
+	color: white;
+}
+
+.entrytext {
+	font-weight: bold;
+	font-size: 1.5em;
+	margin: 1em;
+	color: white;
+	text-shadow: 1px 2px #000000;
+}
     </style>
 <?php
 $servername="localhost";
@@ -33,25 +71,48 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$sql = sprintf("INSERT INTO tasks VALUES (\"%s\", \"%s\")", $_REQUEST["title"], $_REQUEST["description"]);
 	if ($conn->query($sql) === TRUE) {
-		echo "New Record Created Successfully";
+		$conn->close();
+		header('Location: /list-task.php');
 	}
 	else {
 		echo "Error: " . $sql . "<br>". $conn->error;
 	}
+	exit();
 }
-
-$conn->close();
 ?>
+<link rel=stylesheet href="/background.css">
   </head>
   <body>
-    <h3>NEW TASK CREATION</h3>
+<div class="banner">
+<img src="/assets/todo_list.png" class=titleimg>
+<div>
+<div class=todotitle>Todo.net</div>
+<div class=maketodotitle>Make your Todo here and let it available everywhere</div>
+</div>
+</div>
+
+<div class="todocontainer">
+<div class="stylestimg" style="left:10%">
+</div>
+<div class="stylestimg" style="margin-left: 50%;right:20%">
+</div>
+
+<div style="position: absolute;  margin: 0 auto;   left: 20%;
+  right: 10%;">
+<div class=entrytext>Please Enter Title and Description</div>
+<div class="createtaskbox">
     <form method="post" action="<?php echo $_SERVER[" PHP_SELF "];?>">
+	<div style="display:flex;">
       <span>Title:</span>
       <input type="text" name="title">
+	</div>
+	<div style="display:flex;">
       <span>Description:</span>
       <textarea name="description"></textarea>
-	<br><br>
+	</div>
+	<br>
 	<input type=submit value=submit>
     </form>
+</div>
   </body>
 </html>
--- list-task.php
+++ list-task.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html>
 <head>
-	<title>Edit Delete Task</title>
+<title>Edit Delete Task</title>
+<link rel=stylesheet href="/background.css">
 <?php
 
 $servername="localhost";
@@ -17,12 +18,16 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if (isset($_REQUEST["delete"])) {
-
-	} else if (isset($_REQUEST["edit"])) {
-
+	if (isset($_REQUEST["DELETE"])) {
+		$sql = sprintf('SET @row_number = -1;');
+		$conn->query($sql);
+		$sql = sprintf("delete from tasks where title=(select title from (SELECT title, (@row_number:=@row_number + 1) AS row_num FROM tasks) as t where row_num=%s)", $_REQUEST["DELETE"]);
+		$conn->query($sql);
+		header('Location: /list-task.php');
+		exit();
+	} else if (isset($_REQUEST["EDIT"])) {
+		exit();
 	}
-	echo var_dump($_REQUEST);
 }
 ?>
 
@@ -36,11 +41,25 @@ table {
     border-collapse: separate;
 }
 </style>
-
 </head>
 <body>
+<div class="banner">
+<img src="/assets/todo_list.png" class=titleimg>
+<div>
+<div class=todotitle>Todo.net</div>
+<div class=maketodotitle>Make your Todo here and let it available everywhere</div></div>
+</div>
 
+<div class="todocontainer">
+<div class="stylestimg" style="left:10%">
+</div>
+<div class="stylestimg" style="margin-left: 50%;right:20%">
+</div>
+<div style="position: absolute;  margin: 0 auto;   left: 20%;
+  right: 10%;">
+<h2>Please validate todo</h2>
 <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+
 <?php
 
 $sql=sprintf("select * from tasks");
@@ -61,8 +80,27 @@ for ($rowIndex = 0; $rowIndex < count($rows); $rowIndex++) {
 echo $resultanthtml . "</table>";
 ?>
 </form>
-
+</div>
+</div>
 
 <?PHP $conn->close(); ?>
+<script> 
+const deleteElems = document.querySelectorAll("button[name=DELETE]");
+for (const deleteElem of deleteElems)
+{
+
+deleteElem.addEventListener("click", function(e) {
+if (confirm('Please confirm deletion') === true) {
+	const formData = new FormData(document.querySelector("form"));
+	fetch("/list-task.php", {
+		method:"POST",
+		body:formData
+	});
+} else {
+	location.href="/list-task.php";
+}
+});
+}
+</script>
 </body>
 </html>
