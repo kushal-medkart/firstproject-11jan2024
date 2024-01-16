133ddd64f2406ee5893e7dd269a76f4d74731ac4
kushal <kushal.p@medkart.in>
2024-01-16 14:05:00 +0530

    login page but nologin page

--- index.php
+++ index.php
@@ -6,7 +6,39 @@
 </head>
 <body>
     <!-- redirect to create-task page -->
-<script >location.href="/create-task.php"</script>
+
+    <?php
+/*
+    function printError() {
+	header('WWW-Authenticate: Basic realm="My Realm"');
+	header('HTTP/1.0 401 Unauthorized');
+	exit;
+}
+
+
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+	printError();
+} else {
+	$sql = sprintf("select * from register where user='%s'", $_SERVER['PHP_AUTH_USER']);
+	$result = $mydb->query($sql);
+	$row = $result->fetchArray();
+
+	// count number entry
+	if ($row) {
+		if ($row['password'] != $_SERVER['PHP_AUTH_PW'])
+			printError();
+		// when password is equal do nothing
+	}
+	else {
+		if (strlen($_SERVER['PHP_AUTH_USER']) > 40 || strlen($_SERVER['PHP_AUTH_PW']) > 40)
+			printError();
+
+		$sql = sprintf("insert into register values ('%s', '%s')", $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+		$mydb->query($sql);
+	}
+}*/
+?>
+<script >location.href="/create-task.php"</script> 
 </div>
 </body>
 </html>
